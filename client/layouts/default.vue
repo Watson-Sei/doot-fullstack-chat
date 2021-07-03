0089dc0a3f4ded6dbb8ffccf8a90bef8f6e7338c
Watson-Sei <55475145+Watson-Sei@users.noreply.github.com>
2021-07-03 15:33:14 +0900

    sidebar and create default layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -4,9 +4,23 @@
       <v-navigation-drawer
         app
       >
+        <v-container>
+          <v-list dense nav>
+            <v-list-item v-for="history in historys"
+              :to="`/rooms/` + history.room.id + `/`"
+              :key="history.id"
+            >
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ history.user.email }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-container>
       </v-navigation-drawer>
       <v-app-bar app>
-        <v-toolbar-title>User 1</v-toolbar-title>
+        <v-toolbar-title>Romm {{ this.$route.params.id }}</v-toolbar-title>
       </v-app-bar>
     </div>
     <div v-else>
@@ -31,6 +45,50 @@
 export default {
   data() {
     return {
+      historys: [
+        {
+          "id": 3,
+          "room": {
+              "id": 2,
+              "name": null,
+              "created_at": "2021-07-01T01:01:52.360Z",
+              "updated_at": "2021-07-01T01:01:52.360Z"
+          },
+          "user": {
+              "id": 2,
+              "provider": "email",
+              "uid": "test2@example.com",
+              "allow_password_change": false,
+              "name": null,
+              "nickname": null,
+              "image": null,
+              "email": "test2@example.com",
+              "created_at": "2021-06-30T12:30:28.746Z",
+              "updated_at": "2021-06-30T12:30:28.810Z"
+          }
+      },
+      {
+          "id": 5,
+          "room": {
+              "id": 3,
+              "name": null,
+              "created_at": "2021-07-02T02:55:35.432Z",
+              "updated_at": "2021-07-02T02:55:35.432Z"
+          },
+          "user": {
+              "id": 3,
+              "provider": "email",
+              "uid": "test3@example.com",
+              "allow_password_change": false,
+              "name": null,
+              "nickname": null,
+              "image": null,
+              "email": "test3@example.com",
+              "created_at": "2021-06-30T12:30:37.164Z",
+              "updated_at": "2021-06-30T12:30:37.228Z"
+          }
+      }
+      ]
     }
   }
 }
